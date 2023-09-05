0254e91c70244cef8d77199149be277427667c7f
rezamohdev <mohammadireza29@gmail.com>
2023-09-05 14:46:12 -0400

    initial commit

---
+++ __tests__/test.index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const fixtures = require('../fixtures');
+
+const MONGO_URL = 'mongodb://localhost:27017/aroundtheus';
+
+// Write your code
+beforeAll(() => {
+
+});
+afterAll(() => {
+
+});
+
+describe('Database tests', () => {
+    beforeEach(() => { });
+    afterEach(() => { });
+});
+
+test('test name', () => { });
